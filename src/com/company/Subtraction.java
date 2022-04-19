19a2b2f3c0526ab3b25f8165a496b48a34fabbbc
honey <shubhamakkicool@gmail.com>
2022-04-19 12:22:28 -0400

    last commit

---
+++ src/com/company/Subtraction.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        int i=10;
+        int j=20;
+        int k=j-i;
+        System.out.print("subtraction of "+j+"-"+i+"="+k);
+    }
+}
