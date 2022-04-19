c2c6dd795d9163381980b818050f160670676971
honey <shubhamakkicool@gmail.com>
2022-04-19 09:39:49 -0400

    Second Commit

---
+++ src/com/company/Addition.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Addition {
+    public static void main(String[] args) {
+        int i=10;
+        int j=20;
+        int k=i+j;
+        System.out.println("Sum of "+i+"+"+j+"="+k);
+    }
+}
--- src/com/company/Test1.java
+++ src/com/company/Test1.java
@@ -3,5 +3,6 @@ package com.company;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
     }
 }
